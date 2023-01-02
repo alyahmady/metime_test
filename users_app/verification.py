51dd6c751821805de7dffb07bf2a074441749cb3
alyahmady <better.aly.ahmady@gmail.com>
2023-01-02 11:22:47 +0330

    feat: update verification tasks to handle phone fields with SMS

--- users_app/verification.py
+++ users_app/verification.py
@@ -14,17 +14,25 @@ def activation_key_generator() -> str:
 
 
 @celery_app.task(ignore_result=True)
-def send_user_verification_code(user: get_user_model()) -> None:
+def send_user_verification_code(
+    user: get_user_model(), user_field: settings.UserIdentifierField
+) -> None:
     if user.is_verified:
         raise ValueError("User is already verified")
 
     activation_key: str = activation_key_generator()
-    email_message = f"""
+    message = f"""
         Your verification code is: {activation_key}
     """
-    user.email_user(
-        settings.VERIFICATION_EMAIL_SUBJECT, email_message, fail_silently=True
-    )
+
+    if user_field == settings.UserIdentifierField.EMAIL:
+        user.email_user(
+            subject=settings.VERIFICATION_EMAIL_SUBJECT,
+            message=message,
+            fail_silently=True,
+        )
+    elif user_field == settings.UserIdentifierField.PHONE:
+        user.sms_user(message=message)
 
     cache.set(
         key=settings.VERIFICATION_CACHE_KEY.format(str(user.id)),
@@ -34,15 +42,23 @@ def send_user_verification_code(user: get_user_model()) -> None:
 
 
 @celery_app.task(ignore_result=True)
-def send_user_reset_password_code(user: get_user_model()):
+def send_user_reset_password_code(
+    user: get_user_model(), user_field: settings.UserIdentifierField
+):
     activation_key = activation_key_generator()
 
-    email_message = f"""
+    message = f"""
         Your password recovery code is: {activation_key}
     """
-    user.email_user(
-        settings.FORGOT_PASSWORD_EMAIL_SUBJECT, email_message, fail_silently=True
-    )
+
+    if user_field == settings.UserIdentifierField.EMAIL:
+        user.email_user(
+            subject=settings.FORGOT_PASSWORD_EMAIL_SUBJECT,
+            message=message,
+            fail_silently=True,
+        )
+    elif user_field == settings.UserIdentifierField.PHONE:
+        user.sms_user(message=message)
 
     cache.set(
         key=settings.FORGOT_PASSWORD_CACHE_KEY.format(str(user.id)),
