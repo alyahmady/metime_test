d0bb8fdfeac07668fba629b14755398ffed05e29
alyahmady <better.aly.ahmady@gmail.com>
2023-01-04 01:06:46 +0330

    test: implement User Update API test cases

--- users_app/tests/__init__.py
+++ users_app/tests/__init__.py
@@ -2,3 +2,4 @@ from .models import CustomUserTestCase
 from .verification import CustomUserVerificationTestCase
 from .password import CustomUserPasswordTestCase
 from .register import UserRegisterAPITestCase
+from .update import UserUpdateAPITestCase
