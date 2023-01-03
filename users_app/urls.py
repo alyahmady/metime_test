b38a3826184e8b1d89edfeb9f18fc45301b7e88b
alyahmady <better.aly.ahmady@gmail.com>
2023-01-03 22:04:43 +0330

    feat: add User Update API route

--- users_app/urls.py
+++ users_app/urls.py
@@ -4,4 +4,5 @@ from users_app.views import UserViewSet
 
 urlpatterns = [
     path("", UserViewSet.as_view({"post": "create"}), name="user-register"),
+    path("<int:user_id>", UserViewSet.as_view({"patch": "update"}), name="user-update"),
 ]
