dc3637a7698d807a2c3d6954b4e9982deb620a16
alyahmady <better.aly.ahmady@gmail.com>
2023-01-04 13:49:26 +0330

    feat: add token `iat` validator to `CustomJWTAuthentication` backend

--- auth_app/backend.py
+++ auth_app/backend.py
@@ -1,9 +1,16 @@
+import datetime
+
 from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend
 from django.utils.translation import gettext_lazy as _
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework_simplejwt.exceptions import InvalidToken, AuthenticationFailed
+from rest_framework_simplejwt.exceptions import (
+    InvalidToken,
+    AuthenticationFailed,
+)
 from rest_framework_simplejwt.settings import api_settings
+from rest_framework_simplejwt.tokens import Token
+from rest_framework_simplejwt.utils import datetime_from_epoch
 
 
 class AuthHelper:
@@ -38,6 +45,21 @@ class CustomJWTAuthentication(AuthHelper, JWTAuthentication):
     www_authenticate_realm = "api"
     media_type = "application/json"
 
+    def validate_token_iat(self, validated_token: Token, user: get_user_model()):
+        """
+        Validate `iat` claim, beside other validations, to make sure that `iat`
+         is not before user's last password change date.
+
+        :param validated_token: `Token` class instance of the raw JWT
+        :raise InvalidToken
+        """
+
+        iat_value = validated_token.payload["iat"]
+        iat_time: datetime.datetime = datetime_from_epoch(iat_value)
+
+        if iat_time <= user.last_password_change:
+            raise InvalidToken(_("Token is expired. User must re-login"))
+
     def get_user(self, validated_token):
         try:
             user_id = validated_token[api_settings.USER_ID_CLAIM]
@@ -45,7 +67,7 @@ class CustomJWTAuthentication(AuthHelper, JWTAuthentication):
             raise InvalidToken(_("Token contained no recognizable user identification"))
 
         try:
-            user = self.user_model._default_manager.get(
+            user: get_user_model() = self.user_model._default_manager.get(
                 **{api_settings.USER_ID_FIELD: user_id}
             )
         except self.user_model.DoesNotExist:
@@ -53,5 +75,7 @@ class CustomJWTAuthentication(AuthHelper, JWTAuthentication):
 
         if not self.user_can_authenticate(user):
             raise AuthenticationFailed(_("User is not verified"), code="user_inactive")
+
+        self.validate_token_iat(validated_token, user)
 
         return user
