4d4eba209732482c0b8e02389cb96146010f4458
alyahmady <better.aly.ahmady@gmail.com>
2023-01-04 13:47:23 +0330

    feat: implement `CustomAccessToken`

---
+++ auth_app/tokens.py
@@ -0,0 +1,23 @@
+from django.utils.translation import gettext_lazy as _
+from rest_framework_simplejwt.exceptions import TokenError
+from rest_framework_simplejwt.tokens import AccessToken
+
+
+class CustomAccessToken(AccessToken):
+    def check_iat(self, claim="iat"):
+        """
+        Checks whether token's `iat` claim is present in the token payload or not.
+        If not, token will be invalid.
+
+        Raises a TokenError with a user-facing error message if so.
+        """
+
+        try:
+            self.payload[claim]
+        except KeyError:
+            raise TokenError(_(f"Token has no '{claim}' claim"))
+
+    def verify(self):
+        super().verify()
+
+        self.check_iat()
