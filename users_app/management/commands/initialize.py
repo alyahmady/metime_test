348c83c22fb930103122a89aeb0ae8c637de57f6
alyahmady <better.aly.ahmady@gmail.com>
2023-01-04 18:25:57 +0330

    feat: add `initialize` custom management command to create superuser

---
+++ users_app/management/commands/initialize.py
@@ -0,0 +1,27 @@
+import os
+
+from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand
+from django.db.models import Q
+
+
+# Run this file using "python manage.py initialize" command
+
+
+class Command(BaseCommand):
+    help = "Superuser(admin) creation on first run"
+
+    def handle(self, *args, **options):
+        CustomUser = get_user_model()
+        admin_username = os.getenv("DJANGO_ADMIN_USERNAME", "admin")
+        admin_email = os.getenv("DJANGO_ADMIN_EMAIL", "admin@example.com")
+        admin_password = os.getenv("DJANGO_ADMIN_PASS", "samplepass123")
+
+        if not CustomUser.objects.filter(
+            Q(email=admin_email) | Q(username=admin_username) | Q(is_superuser=True)
+        ).exists():
+            CustomUser.objects.create_superuser(
+                username=admin_username, email=admin_email, password=admin_password
+            )
+
+        self.stdout.write(self.style.SUCCESS("Successfully created Superadmin User"))
